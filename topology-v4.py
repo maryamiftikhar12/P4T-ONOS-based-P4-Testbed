07227ab6a84a2f57c9b2e56d3706bb10eecb9431
Maryam Iftikhar <117350844+maryamiftikhar12@users.noreply.github.com>
2024-06-12 11:32:35 +0500

    Update topology-v4.py

--- topology-v4.py
+++ topology-v4.py
@@ -3,14 +3,13 @@ import argparse
 from mininet.cli import CLI
 from mininet.log import setLogLevel
 from mininet.net import Mininet
-from mininet.node import Host
+from mininet.node import Host, RemoteController
 from mininet.topo import Topo
 from stratum import StratumBmv2Switch
 
 CPU_PORT = 255
 
 class IPv4Host(Host):
-    """Host that can be configured with an IPv4 gateway (default route)."""
 
     def config(self, mac=None, ip=None, defaultRoute=None, lo='up', gw=None,
                **_params):
@@ -32,92 +31,27 @@ class IPv4Host(Host):
 
         self.defaultIntf().updateIP = updateIP
 
-class TaggedIPv4Host(Host):
-    """VLAN-tagged host that can be configured with an IPv4 gateway
-    (default route).
-    """
-    vlanIntf = None
-
-    def config(self, mac=None, ip=None, defaultRoute=None, lo='up', gw=None,
-               vlan=None, **_params):
-        super(TaggedIPv4Host, self).config(mac, ip, defaultRoute, lo, **_params)
-        self.vlanIntf = "%s.%s" % (self.defaultIntf(), vlan)
-        # Replace default interface with a tagged one
-        self.cmd('ip -4 addr flush dev %s' % self.defaultIntf())
-        self.cmd('ip -6 addr flush dev %s' % self.defaultIntf())
-        self.cmd('ip -4 link add link %s name %s type vlan id %s' % (
-            self.defaultIntf(), self.vlanIntf, vlan))
-        self.cmd('ip -4 link set up %s' % self.vlanIntf)
-        self.cmd('ip -4 addr add %s dev %s' % (ip, self.vlanIntf))
-        if gw:
-            self.cmd('ip -4 route add default via %s' % gw)
-
-        self.defaultIntf().name = self.vlanIntf
-        self.nameToIntf[self.vlanIntf] = self.defaultIntf()
-
-        # Disable offload
-        for attr in ["rx", "tx", "sg"]:
-            cmd = "/sbin/ethtool --offload %s %s off" % (
-                self.defaultIntf(), attr)
-            self.cmd(cmd)
-
-        def updateIP():
-            return ip.split('/')[0]
-
-        self.defaultIntf().updateIP = updateIP
-
-    def terminate(self):
-        self.cmd('ip -4 link remove link %s' % self.vlanIntf)
-        super(TaggedIPv4Host, self).terminate()
-
 class TutorialTopo(Topo):
-    """2x2 fabric topology with IPv4 hosts"""
+    """Simple topology with one switch and two IPv4 hosts"""
 
     def __init__(self, *args, **kwargs):
         Topo.__init__(self, *args, **kwargs)
 
-        # Leaves
-        # gRPC port 50001
-        leaf1 = self.addSwitch('leaf1', cls=StratumBmv2Switch, cpuport=CPU_PORT)
-        # gRPC port 50002
-        leaf2 = self.addSwitch('leaf2', cls=StratumBmv2Switch, cpuport=CPU_PORT)
+        # Single switch
+        switch = self.addSwitch('s1', cls=StratumBmv2Switch, cpuport=CPU_PORT)
 
-        # Spines
-        # gRPC port 50003
-        spine1 = self.addSwitch('spine1', cls=StratumBmv2Switch, cpuport=CPU_PORT)
-        # gRPC port 50004
-        spine2 = self.addSwitch('spine2', cls=StratumBmv2Switch, cpuport=CPU_PORT)
-
-        # Switch Links
-        self.addLink(spine1, leaf1)
-        self.addLink(spine1, leaf2)
-        self.addLink(spine2, leaf1)
-        self.addLink(spine2, leaf2)
-
-        # IPv4 hosts attached to leaf 1
+        # IPv4 hosts
         h1 = self.addHost('h1', cls=IPv4Host, mac="00:00:00:00:00:1A",
                            ip='172.16.1.1/24', gw='172.16.1.254')
         h2 = self.addHost('h2', cls=IPv4Host, mac="00:00:00:00:00:1B",
                            ip='172.16.1.2/24', gw='172.16.1.254')
-        h3 = self.addHost('h3', cls=TaggedIPv4Host, mac="00:00:00:00:00:1C",
-                           ip='172.16.1.3/24', gw='172.16.1.254', vlan=100)
-        h4 = self.addHost('h4', cls=TaggedIPv4Host, mac="00:00:00:00:00:20",
-                          ip='172.16.2.1/24', gw='172.16.2.254', vlan=200)
-        self.addLink(h1, leaf1)  # port 3
-        self.addLink(h2, leaf1)  # port 4
-        self.addLink(h3, leaf1)  # port 5
-        self.addLink(h4, leaf1)  # port 6
 
-        # IPv4 hosts attached to leaf 2
-        h5 = self.addHost('h5', cls=TaggedIPv4Host, mac="00:00:00:00:00:30",
-                          ip='172.16.3.1/24', gw='172.16.3.254', vlan=300)
-        h6 = self.addHost('h6', cls=IPv4Host, mac="00:00:00:00:00:40",
-                          ip='172.16.4.1/24', gw='172.16.4.254')
-        self.addLink(h5, leaf2)  # port 3
-        self.addLink(h6, leaf2)  # port 4
+        # Host links
+        self.addLink(h1, switch)
+        self.addLink(h2, switch)
 
 def main():
-    net = Mininet(topo=TutorialTopo(), controller=RemoteController, ip="10.3.12.139", port=8181)
+    net = Mininet(topo=TutorialTopo(), controller=RemoteONOSController, ip="10.3.12.139", port=8181)
     net.start()
     CLI(net)
     net.stop()
@@ -125,7 +59,7 @@ def main():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Mininet topology script for 2x2 fabric with stratum_bmv2 and IPv4 hosts')
+        description='Mininet topology script with one switch and two IPv4 hosts')
     args = parser.parse_args()
     setLogLevel('info')
 
